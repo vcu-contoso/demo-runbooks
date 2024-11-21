ad62e0c0ed19284934b15ca8ff9f676e5ac06fc1
campbelljoe <139894116+campbelljoe@users.noreply.github.com>
2024-11-21 12:05:26 -0500

    Added ManageLogicAppSample.ps1

---
+++ scripts/ManageLogicAppSample.ps1
@@ -0,0 +1,37 @@
+Param
+(
+    [Parameter (Mandatory= $true)]
+    [String] $Subscription,
+
+    [Parameter (Mandatory= $true)]
+    [String] $ResourceGroup,
+
+    [Parameter (Mandatory= $true)]
+    [String] $LogicApp,
+
+    [Parameter (Mandatory= $true)]
+    [String[]] $Workflows,
+
+    [Parameter (Mandatory= $true)]
+    [String] $State
+)
+
+# Set the preference to stop the job when an error occurs
+$ErrorActionPreference = "Stop"
+
+# Create the settings paramerter, which consists of a space-separated list of settings tp set the states of the workflows
+# Format is: Workflows.[workflow name].FlowState=[state]
+$settings = ""
+foreach($workflow in $Workflows) {
+    $settings = $settings + " Workflows." + $workflow + ".FlowState=" + $State
+}
+$settings = $settings.Trim()
+
+Write-Output "Subscription: $Subscription"
+Write-Output "ResourceGroup: $ResourceGroup"
+Write-Output "LogicApp: $LogicApp"
+Write-Output "Workflows: $Workflows"
+Write-Output "Settings: $settings"
+Write-Output "State: $State"
+
+"Completed"
